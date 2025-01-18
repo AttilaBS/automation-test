2b56bd1b8fd723016819329ed98d5b12f3243f77
Attila Barcellos <attila.barcellos@ikatec.com.br>
2025-01-18 20:41:51 -0300

    [Fix] Ajustando alguns problemas detectados nos linters

--- config/database.php
+++ config/database.php
@@ -109,7 +109,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            // 'trust_server_certificate' => env(
+            // 'DB_TRUST_SERVER_CERTIFICATE',
+            // 'false'
+            // ),
         ],
 
     ],
@@ -147,7 +150,14 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env(
+                'REDIS_PREFIX',
+                Str::slug(
+                    env('APP_NAME', 'laravel'),
+                    '_'
+                )
+                .'_database_'
+            ),
         ],
 
         'default' => [
--- run-code-check.sh
+++ run-code-check.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
-
-git fetch origin
-
-if git diff --quiet "$BRANCH_NAME" origin/"$BRANCH_NAME"; then
-  echo "Sem novos commits para enviar, pulando os testes."
-  exit 0
-fi
-
-echo "Mudanças detectadas, executando as validações..."
-
 # Exit immediately if a command exits with a non-zero status
 set -e
 
